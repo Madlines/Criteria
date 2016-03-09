54b783a416a8ba976f055887cb072443dc9d6f1a
Aleksander Ciesiołkiewicz <a.ciesiolkiewicz@gmail.com>
2016-03-09 08:58:21 +0100

    + basic configuration

--- tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!file_exists('vendor/autoload.php')) {
-        echo 'Make sure to run `composer install` before running and tests';
-            exit(1);
-}
-require_once 'vendor/autoload.php';
